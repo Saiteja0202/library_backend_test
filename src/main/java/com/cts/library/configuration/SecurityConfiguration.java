5e4529b63e2408061e1b98a9f92272e879c1a2c5
Srikakulapu Sai Teja <saitejadivya2002@gmail.com>
2025-10-09 15:32:20 +0530

    Cros

--- src/main/java/com/cts/library/configuration/SecurityConfiguration.java
+++ src/main/java/com/cts/library/configuration/SecurityConfiguration.java
@@ -33,8 +33,8 @@ public class SecurityConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
-        config.setAllowedOrigins(List.of("https://library-management-angular.onrender.com/"));     
+        
+        config.setAllowedOrigins(List.of("https://library-management-angular.onrender.com"));     
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true); 
